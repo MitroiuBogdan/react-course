9345a6f80c239482bcc76da8097d2a7d1810c328
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2022-11-06 17:53:20 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,27 @@ const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
 function App() {
-  return <h1>Hello react</h1>;
+  let message = "Bye there!";
+
+  if (Math.random() > 0.5) {
+    message = "Hello there!";
+  } else {
+    message = "The number is lower than 0.5";
+  }
+
+  return <h1>{message}</h1>;
 }
 
-root.render(<App />);
+function PrintTimeComponent() {
+  const date = new Date();
+  const time = date.toLocaleTimeString();
+
+  return <h1>{time}</h1>;
+}
+
+function InputComponent() {
+  return <input type={"number"} />;
+}
+
+root.render(<PrintTimeComponent />);
+root.render(<InputComponent />);
